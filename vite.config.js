fdd834a79997ffc5f4291a94dadfcefaf0c23f17
medicsera <anton.mirko.0550@gmail.com>
2025-11-09 16:29:35 +0700

    nothing

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/vue-v3/',
   plugins: [vue()],
   resolve: {
     alias: {
@@ -19,6 +20,5 @@ export default defineConfig({
         `
       }
     }
-  },
-  base: '/vue-v3/'
+  }
 })
