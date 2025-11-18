64795691aa41bf7984151f32507e6cb154eb83f8
medicsera <anton.mirko.0550@gmail.com>
2025-11-18 15:20:19 +0700

    moved the page from the task to a separate TodoView file

---
+++ src/views/TodoView.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="page">
+    <div class="page__container">
+      <SearchBar v-model="searchQuery" @open-modal="showModal = true" />
+      <TaskList
+        :tasks="sortedTasks"
+        @toggle-completed="toggleCompleted"
+        @delete-task="deleteTask"
+        @edit-task="editTask"
+      />
+      <TaskForm
+        v-if="showModal"
+        @close="showModal = false"
+        @add-task="addTask"
+      />
+    </div>
+    <ThemeToggle />
+  </div>
+</template>
+
+<script>
+import { ref, computed, watch } from "vue";
+import TaskList from "../components/TaskList.vue";
+import TaskForm from "../components/TaskForm.vue";
+import SearchBar from "../components/SearchBar.vue";
+import ThemeToggle from "../components/ThemeToggle.vue";
+import { useTodoStore } from "../stores/useTodoStore";
+import { useRouter } from "vue-router";
+import { useUserStore } from "../stores/useUserStore";
+
+export default {
+  components: { TaskList, TaskForm, SearchBar, ThemeToggle },
+  setup() {
+    const showModal = ref(false);
+    const searchQuery = ref("");
+    const todoStore = useTodoStore();
+    const userStore = useUserStore();
+    const router = useRouter();
+
+    function addTask(task){
+      todoStore.addTask(task)
+      showModal.value = false;
+    }
+
+    function editTask({id,task}){
+      todoStore.editTask({id,task});
+    }
+
+    function deleteTask(id){
+      if (confirm("Вы точно хотите удалить эту задачу?")){
+        todoStore.deleteTask(id)
+      }
+    }
+
+    function toggleCompleted(id){
+      todoStore.toggleCompleted(id)
+    }
+
+    const sortedTasks = computed(() => {
+      if (!userStore.currentUser) return []
+      
+      let filtered = todoStore.tasks.filter(task => task.email === userStore.currentUser.email)
+
+      if (searchQuery.value) {
+        filtered = filtered.filter((t) =>
+          t.text.toLowerCase().includes(searchQuery.value.toLowerCase())
+        );
+      }
+
+      return filtered.slice().sort((a, b) => {
+        if (a.completed === b.completed) return 0;
+        return a.completed ? 1 : -1;
+      });
+    });
+   
+
+    return {
+      showModal,
+      searchQuery,
+      sortedTasks,
+      addTask,
+      deleteTask,
+      toggleCompleted,
+      editTask,
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss"></style>
