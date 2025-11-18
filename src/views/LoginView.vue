44d894619f082194e74a39d1ab7e4f9e004378b0
medicsera <anton.mirko.0550@gmail.com>
2025-11-18 18:29:10 +0700

    the styles of the login page have been changed

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,26 +1,28 @@
 <template>
   <div class="page">
-    <div class="login__container">
+    <div class="login-reg__container">
       <h1>Вход</h1>
-      <form class="login-form" @submit.prevent="login">
+      <form class="form" @submit.prevent="login">
         <input
-          class="login-form-input"
+          class="form-input"
           v-model="email"
           placeholder="Email"
           required
         />
         <input
-          class="login-form-input"
+          class="form-input"
           v-model="password"
           placeholder="Password"
           required
         />
-        <button class="login-form-btn" type="submit">Войти</button>
+        <button class="form-btn" type="submit">Войти</button>
         <p class="error" v-if="error">{{ error }}</p>
       </form>
-      <div class="to-register">
+      <div class="toggle-auth">
         <p>Нет аккаунта?</p>
-        <router-link class="link" to="/register"> Зарегистрироваться</router-link>
+        <router-link class="link" to="/register">
+          Зарегистрироваться</router-link
+        >
       </div>
     </div>
     <ThemeToggle />
@@ -53,65 +55,9 @@ async function login() {
 </script>
 
 <style lang="scss" scoped>
-.login__container {
-  @include border;
+h1 {
   display: flex;
-  flex-direction: column;
-  background: $color-dark-purple;
-  width: 500px;
-  height: 600px;
-  margin-top: 8%;
-  padding: 24px;
-
-  h1 {
-    display: flex;
-    justify-content: center;
-    color: $color-white;
-  }
-
-  .login-form {
-    display: flex;
-    flex-direction: column;
-    margin: 20px;
-
-    &-input {
-      @include border;
-      height: 40px;
-      margin: 10px 0px;
-      padding-left: 10px;
-      font-size: 1.2em;
-    }
-
-    &-btn {
-      @include border;
-      height: 50px;
-      margin: 30px 0px;
-      background: $color-light-purple;
-      font-size: 1.3em;
-    }
-
-    .error {
-    display: flex;
-    justify-content: center;
-      color: $color-red;
-      font-size: 1.4em;
-    }
-  }
-
-  .to-register{
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin-top: auto;
-    font-size: 1.3em;
-    color: $color-white;      
-
-    .link{
-        color: $color-light-purple;
-        text-decoration: none;
-        margin-left: 10px;
-    }
-  }
-  
+  justify-content: center;
+  color: $color-white;
 }
 </style>
