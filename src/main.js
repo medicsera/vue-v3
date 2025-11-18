89dfbe4a992ab028a84a0db94978c575332cb776
medicsera <anton.mirko.0550@gmail.com>
2025-11-18 15:21:14 +0700

    the vue router is configured

--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./scss/main.scss";
 import { createPinia } from "pinia";
-import piniaPersist from "pinia-plugin-persistedstate"
+import piniaPersist from "pinia-plugin-persistedstate";
+import router from './router';
 
 const pinia = createPinia()
 pinia.use(piniaPersist)
 
-createApp(App).use(pinia).mount("#app");
+createApp(App).use(pinia).use(router).mount("#app");
 
---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import { createRouter, createWebHistory } from "vue-router";
+import LoginView from "../views/LoginView.vue";
+import TodoView from "../views/TodoView.vue";
+import { useUserStore } from "../stores/useUserStore";
+import { def } from "@vue/shared";
+
+const routes = [
+    { path: '/', redirect: '/login' },
+    { path: '/login', component: LoginView},
+    { path: '/todo', component: TodoView, meta: {requiresAuth: true} }
+];
+
+const router = createRouter({
+    history: createWebHistory('/vue-v3'),
+    routes
+});
+
+router.beforeEach((to,from,next) => {
+    const user = useUserStore();
+    if (to.meta.requiresAuth && !user.currentUser){
+        next('/login');
+    }   else {
+        next();
+    }
+});
+
+export default router;
